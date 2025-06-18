1d76d2e973cfaf5d827e24fafeb2657ba754e29c
giulyko00 <giulio.corbelli@outlook.it>
2025-06-18 16:27:32 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: "Portfolio Backtester",
@@ -21,7 +22,10 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body>{children}</body>
+      <body>
+        <Analytics />
+        {children}
+      </body>
     </html>
   )
 }
